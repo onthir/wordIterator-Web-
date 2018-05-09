18cdf9b6b50ae7adcb12a7fff884dcd7dcfdc8f2
onthir <jny.bbk1@gmail.com>
2018-05-09 21:53:30 +0545

    STATIC FIX 3

--- wordlinkhack/settings.py
+++ wordlinkhack/settings.py
@@ -130,4 +130,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default']=db_from_env
+DATABASES['default'].update(db_from_env)
